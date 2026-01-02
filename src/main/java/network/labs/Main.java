648752f305d977f2eb70eff6a49f83d6c6a1cfc2
bolk <pank_rok_89@mail.ru>
2026-01-02 22:57:00 +0300

    build: add Maven configuration with slf4j and logback

--- src/main/java/network/labs/Main.java
+++ src/main/java/network/labs/Main.java
@@ -1,9 +1,18 @@
 package network.labs;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+
+    private final static Logger log = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
+        
+
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
@@ -12,6 +21,7 @@ public class Main {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
+            log.info("i = " + i);
         }
     }
 }
